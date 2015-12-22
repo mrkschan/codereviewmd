21afb876c42d3fd1a17e020a05dbd310c702c858
KS Chan <mrkschan@gmail.com>
2015-12-23 01:15:15 +0800

    Add some TODOs

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/webhook/pullrequest/', function(request, response) {
+  // TODO: Read CODEREVIEW.md at the root of repo
+  // TODO: HTTP-POST a new checklist onto the PR
+  // TODO: Use async task to create a new checklist
+  response.render('pages/index');
+});
+
+// TODO: Read Github access token from environment
+// TODO: Configure repo location
+// TODO: Setup webhook on Github
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
