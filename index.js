fc71ed37c7db112e0fb7476b947a4bbdeae35829
KS Chan <mrkschan@gmail.com>
2015-12-25 16:58:51 +0800

    Register webhook on Github to receive Pull Request events

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+var crypto = require('crypto');
 var express = require('express');
+var https = require('https');
+var querystring = require('querystring');
 var util = require('util');
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// Github username and access token used for Basic Auth.
+var hostname = process.env.HOSTNAME || 'localhost';
+
+// Prepare shared secret.
+var seed = process.env.SEED || null;
+var buf = (seed)? new Buffer(seed, 'ascii')
+                : new Buffer(crypto.randomBytes(8), 'binary');
+var secret = crypto.createHash('md5').update(buf.toString('binary'))
+                                     .digest('hex')
+
+// Collect Github config.
 var username = process.env.GITHUB_USERNAME || null;
 var token = process.env.ACCESS_TOKEN || null;
 var repo = process.env.GITHUB_REPO || null;
@@ -34,6 +46,65 @@ if (!repo) {
   process.exit(1);
 }
 
+ var data = JSON.stringify({
+ 'name': 'web',
+ 'active': true,
+ 'events': [
+   'pull_request'
+ ],
+ 'config': {
+   'url': util.format('https://%s/webhook/pullrequest/', hostname),
+   'content_type': 'json',
+   'secret': secret
+ }
+});
+
+var req = https.request({
+  hostname: 'api.github.com',
+  path: util.format('/repos/%s/%s/hooks', username, repo),
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length,
+    'User-Agent': util.format('codereviewmd by %s', username)
+  },
+  auth: util.format('%s:%s', username, token)
+}, function(res) {
+  var buf = '';
+  res.on('data', function(chunk) {
+    buf += chunk;
+  });
+  res.on('end', function() {
+    var succeed = false;
+
+    if (res.statusCode === 201) {
+      succeed = true;
+    } else if (res.statusCode === 422) {
+      var msg = JSON.parse(buf).errors[0].message;
+      if (msg.toLowerCase().indexOf('already exists') > -1) {
+        succeed = true;
+      }
+    }
+
+    if (!succeed) {
+      var msg = 'Failed to create webhook: HTTP%s - %s';
+      console.error(util.format(msg, res.statusCode, buf));
+
+      process.exit(1);
+    }
+  });
+});
+
+req.on('error', function(e) {
+  var msg = 'Failed to create webhook: %s';
+  console.error(util.format(msg, e.message));
+
+  process.exit(1);
+});
+
+req.write(data);
+req.end();
+
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -50,8 +121,6 @@ app.get('/webhook/pullrequest/', function(request, response) {
   // TODO: Use async task to create a new checklist
   response.render('pages/index');
 });
-
-// TODO: Setup webhook on Github
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
